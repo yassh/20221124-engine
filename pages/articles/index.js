60027d0185e1716c1a9abc5bff3e0ab9214f7f45
yassh <yassh32@outlook.jp>
2022-11-19 23:23:54 +0900

    記事一覧ページを実装

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,7 +1,26 @@
-const Page = () => {
+import { microcmsClient } from "../../lib/microcmsClient";
+
+export const getStaticProps = async () => {
+  const data = await microcmsClient.get({
+    endpoint: "articles",
+  });
+
+  return {
+    props: {
+      articles: data.contents,
+    },
+  };
+};
+
+const Page = ({ articles }) => {
   return (
     <>
       <h1>記事一覧</h1>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>{article.title}</li>
+        ))}
+      </ul>
     </>
   );
 };
